c3c32b713297e8dea595619bb2c4b9ae2a327b8b
Vladimír Matěna <vlada.matena@gmail.com>
2019-05-16 15:14:50 +0000

    Move non-unittests to utils package
    - This enables automatic execution of all tests in tests package

---
+++ firmware/sl1fw/tests/test_exposure.py
@@ -0,0 +1,46 @@
+import unittest
+from mock import Mock
+import gettext
+import os
+
+try:
+    gettext.install('sl1fw', unicode=1)
+except:
+    gettext.install('sl1fw')
+
+import libExposure
+import libConfig
+
+
+class TestExposure(unittest.TestCase):
+    PROJECT = os.path.join(os.path.dirname(__file__), "samples/empty-sample.sl1")
+
+    def setUp(self):
+        hwConfig = libConfig.HwConfig()
+
+        self.config = libConfig.PrintConfig(hwConfig)
+        display = Mock()
+        hw = Mock()
+        hw.getUvLedState.return_value = (False, 0)
+        screen = Mock()
+        screen.blitImg.return_value = 100
+        self.exposure = libExposure.Exposure(hwConfig, self.config, display, hw, screen)
+
+    def test_exposure_init(self):
+        pass
+
+    def test_exposure_load(self):
+        self.config.parseFile(TestExposure.PROJECT)
+        self.exposure.setProject(TestExposure.PROJECT)
+        self.exposure.loadProject()
+
+    def test_exposure_start_stop(self):
+        self.test_exposure_load()
+
+        self.exposure.start()
+        self.exposure.doExitPrint()
+        self.exposure.waitDone()
+
+
+if __name__ == '__main__':
+    unittest.main()
--- firmware/sl1fw/tests/test_libConfig.py
+++ firmware/sl1fw/tests/test_libConfig.py
@@ -1,6 +1,7 @@
 import unittest
 import gettext
 import os
+import logging
 
 try:
     gettext.install('sl1fw', unicode=1)
@@ -8,6 +9,89 @@ except:
     gettext.install('sl1fw')
 
 from libConfig import *
+import defines
+
+logging.basicConfig(format = "%(asctime)s - %(levelname)s - %(name)s - %(message)s", level = logging.DEBUG)
+
+
+class TestHardwareConfig(unittest.TestCase):
+    def setUp(self):
+        defines.hwConfigFile = os.path.join(os.path.dirname(__file__), "samples/hardware.cfg")
+        from shutil import copyfile
+        copyfile(defines.hwConfigFile, "hwconfig.test")
+        self.hwConfig = HwConfig(defines.hwConfigFile)
+
+    def tearDown(self):
+        os.remove("hwconfig.test")
+
+    def test_read(self):
+        self.hwConfig.logAllItems()
+        self.hwConfig.logFile()
+
+        self.assertTrue(self.hwConfig.coverCheck, "Test cover check read")
+        self.assertFalse(self.hwConfig.calibrated, "Test calibrated read")
+        self.assertEqual(self.hwConfig.layerTowerHop, 0, "Test layerTowerHop read")
+
+    def test_write(self):
+        towerHeight = "1024"
+        self.hwConfig.update(towerheight = towerHeight)
+
+        self.assertEqual(self.hwConfig.towerHeight, 1024, "Check towerHeight is set")
+
+        self.hwConfig.logAllItems()
+        self.hwConfig.logFile()
+        logging.info(self.hwConfig.getSourceString())
+        self.assertTrue(self.hwConfig.writeFile("hwconfig.test"), "Write config file")
+
+
+class TestPrintConfig(unittest.TestCase):
+    def setUp(self):
+        defines.hwConfigFile = os.path.join(os.path.dirname(__file__), "samples/hardware.cfg")
+        self.hwConfig = HwConfig(defines.hwConfigFile)
+
+    def test_read(self):
+        config = PrintConfig(self.hwConfig)
+        config.logAllItems()
+        config.logFile()
+
+        self.assertEqual(config.projectName, "no project", "Check empty project name")
+
+        config.parseFile(os.path.join(os.path.dirname(__file__), "samples/empty-sample.sl1"))
+
+        self.assertIs(config.zipError, None, "Test for no read errors")
+
+        config.logAllItems()
+        config.logFile()
+
+        self.assertEqual(config.projectName, "empty-sample", "Check projectName")
+        self.assertEqual(config.totalLayers, 20, "Check total layers count")
+
+        logging.info(config.getSourceString())
+        config.update(expTime = "5")
+
+        self.assertEqual(config.expTime, 5, "Check expTime value")
+
+        #config.writeFile("printconfig.txt")
+
+
+class TestNetConfig(unittest.TestCase):
+    def test_read(self):
+        netConfig = NetConfig()
+        netConfig.parseText("""image = RSL-180-318
+        firmware = Gen2-180-319""")
+        netConfig.logAllItems()
+        netConfig.logFile()
+
+        self.assertEqual(netConfig.firmware, "Gen2-180-319", "Check firmware version")
+        self.assertEqual(netConfig.image, "RSL-180-318", "Check image version")
+
+
+class TestFWConfig(unittest.TestCase):
+    def test_read(self):
+        fwConfig = FwConfig(os.path.join(defines.usbUpdatePath + defines.swPath, "defines.py"))
+        fwConfig.logAllItems()
+
+        # TODO: What is this supposed to do?
 
 
 class TestConfigHelper(unittest.TestCase):
