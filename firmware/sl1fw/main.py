796b37faed6275b1b58fbcd3a0f32099c58a9f7f
Lubomír Bláha <tritol@trilogic.cz>
2019-06-25 14:06:06 +0200

    fix: gettext in python3

--- firmware/sl1fw/main.py
+++ firmware/sl1fw/main.py
@@ -4,6 +4,7 @@
 # 2014-2018 Futur3d - www.futur3d.net
 # 2018-2019 Prusa Research s.r.o. - www.prusa3d.com
 
+import os
 import logging
 from systemd.journal import JournalHandler
 import gettext
@@ -16,6 +17,7 @@ except:
     import builtins as builtins
 #endtry
 
+import sl1fw
 from sl1fw import libPrinter
 
 handler = JournalHandler(SYSLOG_IDENTIFIER = 'SL1FW')
@@ -25,9 +27,10 @@ logger.addHandler(handler)
 logger.setLevel(logging.DEBUG)
 
 langs = dict()
+localedir = os.path.join(os.path.dirname(sl1fw.__file__), "locales")
 for lang in ('cs', 'de', 'fr', 'it', 'es', 'pl'):
     try:
-        langs[lang] = gettext.translation('sl1fw', localedir='locales', languages=[lang])
+        langs[lang] = gettext.translation('sl1fw', localedir=localedir, languages=[lang])
     except:
         logger.warning("Translation file for language %s not found.", lang)
     #endtry
@@ -38,7 +41,7 @@ logger.info("Avaiable translations: %s", ", ".join(langs.keys()))
 #langs['cs'].install()
 
 # use system locale settings
-gettext.install('sl1fw', 'locales')
+gettext.install('sl1fw', localedir)
 builtins.N_ = lambda x: x
 
 printer = libPrinter.Printer()
