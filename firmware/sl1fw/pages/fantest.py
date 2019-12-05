24a9d3dae712aee1265587b80af71558e29c3f8e
Filip Kotoucek <koty.filip@gmail.com>
2019-12-05 11:36:09 +0100

    New images added + typo fixed

--- firmware/sl1fw/pages/fantest.py
+++ firmware/sl1fw/pages/fantest.py
@@ -26,7 +26,7 @@ class PageFanTest(Page):
 
     def show(self):
         self.items.update({
-            'text' : _("Make sure all fan air vens are clean and not blocked."),
+            'text' : _("Make sure all fan air vents are clean and not blocked."),
             'no_back' : True})
         super(PageFanTest, self).show()
     #enddef
