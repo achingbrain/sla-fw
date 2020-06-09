2ed4a520722f3e9dea46e0d086227a542914af48
Filip Kotoucek <filip.kotoucek@prusa3d.cz>
2020-06-09 14:45:25 +0200

    about-us: updated company name and year

--- firmware/sl1fw/pages/about.py
+++ firmware/sl1fw/pages/about.py
@@ -16,10 +16,10 @@ class PageAbout(Page):
         super(PageAbout, self).__init__(display)
         self.pageUI = "about"
         self.items.update({
-                'line1' : "2018-2019 Prusa Research s.r.o.",
+                'line1' : "2020 Prusa Research a.s.",
                 'line2' : defines.aboutURL,
 #                'qr' : "https://www.prusa3d.com",
-                'qr' : "MECARD:N:Prusa Research s.r.o.;URL:www.prusa3d.com;EMAIL:info@prusa3d.com;;",
+                'qr' : "MECARD:N:Prusa Research a.s.;URL:www.prusa3d.com;EMAIL:info@prusa3d.com;;",
                 'about_url': defines.aboutURL
                 })
     #enddef
