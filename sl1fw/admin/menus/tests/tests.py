69bd71fd3cb27c1a9c72e58f4f00884e1fb152a2
Vladimír Matěna <vlada.matena@gmail.com>
2021-02-16 23:32:13 +0100

    Add option to send mqtt data to admin tests

--- sl1fw/admin/menus/tests/tests.py
+++ sl1fw/admin/menus/tests/tests.py
@@ -6,17 +6,19 @@ import logging
 
 from sl1fw.admin.control import AdminControl
 from sl1fw.admin.items import AdminAction
-from sl1fw.admin.menu import AdminMenu
 from sl1fw.admin.menus.admin_api_test import TestMenu
+from sl1fw.admin.menus.dialogs import Info
 from sl1fw.admin.menus.tests.test_error_types import TestErrorTypesMenu
 from sl1fw.admin.menus.tests.test_errors import TestErrorsMenu
 
 from sl1fw.admin.menus.tests.test_hardware import TestHardwareMenu
 from sl1fw.admin.menus.tests.test_wizards import TestWizardsMenu
+from sl1fw.admin.safe_menu import SafeAdminMenu
 from sl1fw.libPrinter import Printer
+from sl1fw.functions.system import send_printer_data
 
 
-class TestsMenu(AdminMenu):
+class TestsMenu(SafeAdminMenu):
     def __init__(self, control: AdminControl, printer: Printer):
         super().__init__(control)
         self.logger = logging.getLogger(__name__)
@@ -29,3 +31,9 @@ class TestsMenu(AdminMenu):
         self.add_item(AdminAction("Hardware", lambda: self.enter(TestHardwareMenu(self._control, self._printer))))
         self.add_item(AdminAction("Admin API test", lambda: self.enter(TestMenu(self._control))))
         self.add_item(AdminAction("Touchscreen test", self._control.touchscreen_test))
+        self.add_item(AdminAction("Send wizard data", self.send_printer_data))
+
+    @SafeAdminMenu.safe_call
+    def send_printer_data(self):
+        send_printer_data(self._printer.hw, self._printer.hwConfig)
+        self._control.enter(Info(self._control, "Data send"))
