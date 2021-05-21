50923d5b9aafd27cb29fc5d892e43333b2fc3622
Vladimir Matena <vladimir.matena@prusa3d.cz>
2021-05-21 15:50:30 +0200

    Make sound test check async
    Avoid running the check while something else has failed.
    
    Fixes: SL1SW-1065

--- sl1fw/wizard/checks/speaker.py
+++ sl1fw/wizard/checks/speaker.py
@@ -2,33 +2,35 @@
 # Copyright (C) 2020 Prusa Research a.s. - www.prusa3d.com
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from threading import Event
+from asyncio import Event, AbstractEventLoop, get_running_loop
+from functools import partial
 from typing import Optional
 
 from sl1fw.errors.errors import SoundTestFailed
 from sl1fw.states.wizard import WizardState
 from sl1fw.wizard.actions import UserActionBroker, PushState
-from sl1fw.wizard.checks.base import SyncCheck, WizardCheckType
+from sl1fw.wizard.checks.base import Check, WizardCheckType
 from sl1fw.wizard.setup import Configuration
 
 
-class SpeakerTest(SyncCheck):
+class SpeakerTest(Check):
     def __init__(self):
         super().__init__(
             WizardCheckType.MUSIC, Configuration(None, None), [],
         )
         self.result: Optional[bool] = None
-        self._user_event = Event()
+        self._user_event: Optional[Event] = None
 
-    def task_run(self, actions: UserActionBroker):
+    async def async_task_run(self, actions: UserActionBroker):
+        self._user_event = Event()
         self.result = None
         self._user_event.clear()
 
-        actions.report_audio.register_callback(self.user_callback)
+        actions.report_audio.register_callback(partial(self.user_callback, get_running_loop()))
         push_state = PushState(WizardState.TEST_AUDIO)
         actions.push_state(push_state)
 
-        self._user_event.wait()
+        await self._user_event.wait()
 
         actions.drop_state(push_state)
         actions.report_audio.unregister_callback()
@@ -36,6 +38,6 @@ class SpeakerTest(SyncCheck):
             self._logger.error("Sound test failed")
             raise SoundTestFailed()
 
-    def user_callback(self, result: bool):
+    def user_callback(self, loop: AbstractEventLoop, result: bool):
         self.result = result
-        self._user_event.set()
+        loop.call_soon_threadsafe(self._user_event.set)
